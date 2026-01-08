66349d4dd9247bcaa8f68a96cb976f49f667ea31
JuddL44 <businessjudd@gmail.com>
2026-01-08 13:44:05 -0600

    MenuItem, Comment, and User models

---
+++ server/server/Domain/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public int MenuItemId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ server/server/Domain/Entities/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+
+namespace server.Domain.Entities
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Icon { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+    }
+}
